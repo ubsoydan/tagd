fd6c2815566d481a1aa16d012580ebf58ba83f41
Umut Berkay Soydan <ubsoydan@protonmail.com>
2023-06-16 20:37:34 +0300

    add: integrate redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.css";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
+const store = configureStore({
+    reducer: authReducer,
+});
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
